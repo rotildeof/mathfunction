e5e499cffc637a10ced16ffcb0178132880cecb9
rotildeof <49618478+rotildeof@users.noreply.github.com>
2020-12-31 00:44:46 +0900

    Update mathfunction.cxx

--- mathfunction.cxx
+++ mathfunction.cxx
@@ -78,7 +78,7 @@ bool mathfunc::is_prime(uint64_t n) {
   if (n == 0 || n == 1) return false;
   if (n == 2 || n == 3) return true;
   if (n % 2 == 0 || n % 3 == 0) return false;
-  for (uint64_t i = 5; i * i < n; i += 6) {
+  for (uint64_t i = 5; i * i <= n; i += 6) {
     if (n % i == 0) return false;
     if (n % (i + 2) == 0) return false;
   }
